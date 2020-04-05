cdd4fde4ccc49975284f7ebcf7e1dcfbf1d764ff
ksksn <48342231+ksksn@users.noreply.github.com>
2020-04-05 00:07:22 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const request = require('request');
 const cheerio = require('cheerio');
 var app = express();
 
-function covidCount(){
-    var statusJSON;
+app.get("/countryStats", (req, res, next) => {
+    var status;
     request('https://www.mohfw.gov.in/', (error,response,html) => {
     var $ = cheerio.load(html);
 
@@ -13,24 +13,41 @@ function covidCount(){
     const deathCases = $('.bg-red');
     const migratedCases = $('.bg-orange');
 
-    var status = {
+    status = {
         "Active Cases" : activeCases.children('strong').text(),
         "Cured Cases" : curedCases.children('strong').text(),
         "Death Cases" : deathCases.children('strong').text(),
         "Migrated Cases" : migratedCases.children('strong').text()
     };
-    statusJSON = JSON.stringify(status);
-    console.log(statusJSON);
-});
-return statusJSON;
-}
 
-app.get("/url", (req, res, next) => {
-
-    res.type('json');
-    res.json(covidCount());
+    res.json(status);
+}); 
 });
 
+app.get("/stateStats", (req, res, next) => {
+
+    request('https://www.mohfw.gov.in/', (error,response,html) => {
+    var $ = cheerio.load(html);
+    var stateWiseCount = {};
+    $('#state-data > div > div > div > div > table > tbody > tr').each((index, element) => {
+        
+     var states = $($(element).find('td')[1]).text();
+     var totalCasesCount = $($(element).find('td')[2]).text();
+     var curedCasesCount = $($(element).find('td')[3]).text();
+     var deathCasesCount = $($(element).find('td')[4]).text();
+ 
+     //console.log(curedCasesCount);
+     stateWiseCount[index]= {
+         "State" : $($(element).find('td')[1]).text(),
+         "Total Cases" : $($(element).find('td')[2]).text(),
+         "Cured Cases" : $($(element).find('td')[3]).text(),
+         "Death Cases" : $($(element).find('td')[4]).text()
+     };
+     });
+    
+    res.json(stateWiseCount);
+}); 
+});
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
